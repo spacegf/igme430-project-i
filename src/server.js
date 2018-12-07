f9cebe941c8991191d1c3f7d7146e0db68c954fb
spacegf <theironskies@gmail.com>
2018-12-07 00:22:33 -0500

    CI Test

--- src/server.js
+++ src/server.js
@@ -4,16 +4,13 @@ const urban = require('urban-dictionary');
 const fs = require('fs');
 const http = require('http');
 const querystring = require('querystring');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 // examples, defaults
-let savedExpressions = [{ "adj": "aback", "noun": "anus" }, { "adj": "aback", "noun": "arsehole" }, { "adj": "abaft", "noun": "anus" }];
-var app = express();
+const savedExpressions = [{ adj: 'aback', noun: 'anus' }, { adj: 'aback', noun: 'arsehole' }, { adj: 'abaft', noun: 'anus' }];
+const app = express();
 
-const randInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const randInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 // collegiate dictionary key, no thesaurus access
 const merriamApiKey = 'b02427b8-f458-40e7-b79d-77d184fed234';
@@ -21,7 +18,7 @@ const merriamApiKey = 'b02427b8-f458-40e7-b79d-77d184fed234';
 app.use(express.json()); // thank you express
 
 app.use(express.urlencoded({
-  extended: true
+  extended: true,
 }));
 
 // Static files served from the client folder.
@@ -29,24 +26,24 @@ app.use('/assets', express.static('client'));
 
 // Base URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/index.html'));
+  res.sendFile(path.join(`${__dirname}/../client/index.html`));
 });
 
 // Get a random noun
 app.get('/nouns', (req, res) => {
-  const nouns = JSON.parse(fs.readFileSync(__dirname + '/nouns.json'));
+  const nouns = JSON.parse(fs.readFileSync(`${__dirname}/nouns.json`));
   const nounIndex = randInt(0, nouns.noun.length);
   res.send({
-    'noun': nouns.noun[nounIndex]
+    noun: nouns.noun[nounIndex],
   });
 });
 
 // Get a random adjective
 app.get('/adjectives', (req, res) => {
-  const adjectives = JSON.parse(fs.readFileSync(__dirname + '/adjectives.json'));
+  const adjectives = JSON.parse(fs.readFileSync(`${__dirname}/adjectives.json`));
   const adjIndex = randInt(0, adjectives.adjectives.length);
   res.send({
-    'adjective': adjectives.adjectives[adjIndex]
+    adjective: adjectives.adjectives[adjIndex],
   });
 });
 
@@ -54,25 +51,25 @@ app.get('/adjectives', (req, res) => {
 app.get('/lookup/urban', (req, res) => {
   if (typeof req.query.word === 'undefined') {
     res.status(400).send({
-      'id': 'MissingParam',
-      'message': 'Missing "word" URL query parameter.'
+      id: 'MissingParam',
+      message: 'Missing "word" URL query parameter.',
     });
   } else {
     const defLimit = (typeof req.query.limit !== 'undefined') ? req.query.limit : 3;
     urban.term(req.query.word).then((result) => {
-      let definitions = [];
+      const definitions = [];
       result.entries.forEach((entry, index) => {
         if (index < defLimit) {
           definitions.push(entry.definition);
         }
       });
       res.send({
-        'definition': definitions
+        definition: definitions,
       });
-    }).catch((error) => {
+    }).catch(() => {
       res.status(500).send({
-        'id': 'UrbanDictionaryError',
-        'message': 'Error retrieving from Urban Dictionary.'
+        id: 'UrbanDictionaryError',
+        message: 'Error retrieving from Urban Dictionary.',
       });
     });
   }
@@ -81,13 +78,13 @@ app.get('/lookup/urban', (req, res) => {
 app.get('/lookup/merriam', (req, res) => {
   if (typeof req.query.word === 'undefined') {
     res.status(400).send({
-      'id': 'MissingParam',
-      'message': 'Missing "word" URL query parameter.'
+      id: 'MissingParam',
+      message: 'Missing "word" URL query parameter.',
     });
   } else {
     const defLimit = (typeof req.query.limit !== 'undefined') ? req.query.limit : 3;
-    const query = querystring.stringify({ 'key': merriamApiKey });
-    const word = req.query.word;
+    const query = querystring.stringify({ key: merriamApiKey });
+    const { word } = req.query;
     http.get(`http://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?${query}`, (result) => {
       let jsonData = '';
       result.on('data', (buffer) => {
@@ -98,13 +95,12 @@ app.get('/lookup/merriam', (req, res) => {
         try {
           const data = JSON.parse(jsonData);
           res.send({
-            'definition': data[0].shortdef.slice(0, defLimit)
+            definition: data[0].shortdef.slice(0, defLimit),
           });
-        }
-        catch (error) {
+        } catch (error) {
           res.status(500).send({
-            'id': 'InvalidJSON',
-            'message': 'Invalid JSON received from Merriam Webster API.'
+            id: 'InvalidJSON',
+            message: 'Invalid JSON received from Merriam Webster API.',
           });
         }
       });
@@ -127,10 +123,12 @@ app.get('/insults', (req, res) => {
   let adj = '';
   let noun = '';
   if (typeof req.query.adj !== 'undefined') {
-    adj = req.query.adj;
+    ({ adj } = req.query);
+    return;
   }
   if (typeof req.query.noun !== 'undefined') {
-    noun = req.query.noun;
+    ({ noun } = req.query);
+    return;
   }
 
   if (!adj && !noun) {
@@ -138,7 +136,7 @@ app.get('/insults', (req, res) => {
     return;
   }
 
-  let found = savedExpressions.filter((element) => {
+  const found = savedExpressions.filter((element) => {
     if (adj && noun) {
       return element.adj === adj && element.noun === noun;
     }
@@ -148,6 +146,7 @@ app.get('/insults', (req, res) => {
     if (noun) {
       return element.noun === noun;
     }
+    return false;
   });
   // success
   if (found) {
@@ -160,29 +159,29 @@ app.get('/insults', (req, res) => {
 app.post('/insults', (req, res) => {
   if (req.body.adj && req.body.noun) {
     savedExpressions.push({
-      'adj': req.body.adj,
-      'noun': req.body.noun
+      adj: req.body.adj,
+      noun: req.body.noun,
     });
     res.status(201).send('Insult saved successfully.');
   } else {
     res.status(400).send({
-      'id': 'MissingParams',
-      'message': 'Missing adjective or noun parameters to save.'
+      id: 'MissingParams',
+      message: 'Missing adjective or noun parameters to save.',
     });
   }
 });
 
 app.delete('/insults', (req, res) => {
   if (req.body.adj && req.body.noun) {
-    const removeIndex = savedExpressions.findIndex((element) => {
-      return element === { 'adj': req.body.adj, 'noun': req.body.noun };
+    const removeIndex = savedExpressions.findIndex(element => element === {
+      adj: req.body.adj, noun: req.body.noun,
     });
     savedExpressions.splice(removeIndex, 1);
     res.status(204).send({});
   } else {
     res.status(400).send({
-      'id': 'MissingParams',
-      'message': 'Missing adjective or noun parameters to delete.'
+      id: 'MissingParams',
+      message: 'Missing adjective or noun parameters to delete.',
     });
   }
 });
